bfd8f4dfc9dcb2e454115f603724c6a7557cac46
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-01-01 23:20:28 +0100

    Bump version: 0.5.3 → 0.5.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="romnnn_sphinx_press_theme",
-    version="0.5.3",
+    version="0.5.4",
     url="https://github.com/romnnn/sphinx_press_theme",
     license="MIT",
     author="romnn",
